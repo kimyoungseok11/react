241ff002982ae3e3b6d5b12fc485bff4323c191b
kimyoungseok11 <emaonx2@gmail.com>
2023-02-12 16:14:33 +0900

    23-02-12 아이템 리스트 스타일 정리

--- api-sample/src/components/ItemDetail.js
+++ api-sample/src/components/ItemDetail.js
@@ -1,33 +1,72 @@
 import React from "react";
 import ItemImg from "./ItemImg";
+import styled from "styled-components";
+import itemNameColor from "../lib/itemNameColor";
+
+const ItemDetailBox = styled.div`
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 15px;
+  .slot-name {
+    width: 150px;
+    text-align: center;
+  }
+  .item-info {
+    width: calc(100% - 100px);
+    display: flex;
+    justify-content: space-around;
+    .item-name {
+      width: 300px;
+      text-align: center;
+      color: ${(props) => itemNameColor[props.itemRarity]};
+    }
+    .option {
+      width: 100px;
+    }
+  }
+`;
 
 const ItemDetail = ({ info, category }) => {
   console.log(info, category);
 
-  if (
-    (category === "equipment") |
-    (category === "avatar") |
-    (category === "creature")
-  ) {
+  if (category === "talismans") {
     return (
-      <div>
-        {info.itemName}
-        <ItemImg id={info.itemId} />
-      </div>
+      <ItemDetailBox>
+        <div className="slot-name">탈리스만</div>
+        <div className="item-info">
+          <ItemImg id={info.talisman.itemId} />
+          <div className="item-name">{info.talisman.itemName}</div>
+        </div>
+      </ItemDetailBox>
     );
   } else if (category === "flag") {
     return (
-      <div>
-        {info.itemName}
-        <ItemImg id={info.itemId} />
-      </div>
+      <ItemDetailBox>
+        <div className="slot-name">휘장</div>
+        <div className="item-info">
+          <ItemImg id={info.itemId} />
+          <div className="item-name">{info.itemName}</div>
+        </div>
+      </ItemDetailBox>
     );
   }
   return (
-    <div>
-      {info.talisman.itemName}
-      <ItemImg id={info.talisman.itemId} />
-    </div>
+    <ItemDetailBox itemRarity={info.itemRarity}>
+      <div className="slot-name">
+        {!info.slotName ? "크리쳐" : info.slotName}
+      </div>
+      <div className="item-info">
+        <ItemImg id={info.itemId} />
+        <div className="item-name">{info.itemName}</div>
+        <div className="option">
+          {info.optionAbility
+            ? info.optionAbility
+            : info.growInfo?.options.map((option) => (
+                <span>{option.level} </span>
+              ))}
+        </div>
+      </div>
+    </ItemDetailBox>
   );
 };
 
---
+++ api-sample/src/lib/itemNameColor.js
@@ -0,0 +1,10 @@
+const itemNameColor = {
+  커먼: "#000000",
+  언커먼: "#00D2FC",
+  레어: "#9500F2",
+  유니크: "#E500FF",
+  레전더리: "#FF8C00",
+  에픽: "#FFC300",
+};
+
+export default itemNameColor;
