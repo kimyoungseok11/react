13484dc6153ba6a43e9bc531126fceb48ccefd78
kimyoungseok11 <emaonx2@gmail.com>
2024-02-18 18:12:17 +0900

    24-02-18 css Module, tailwind 학습

---
+++ reactcss/src/App.js
@@ -0,0 +1,16 @@
+import React from "react";
+import Button1 from "./components/Button1";
+import Button2 from "./components/Button2";
+import TailWindComponents from "./components/TailWindComponents";
+
+const App = () => {
+  return (
+    <div>
+      <Button1 />
+      <Button2 />
+      <TailWindComponents />
+    </div>
+  );
+};
+
+export default App;
---
+++ reactcss/src/components/TailWindComponents.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const TailWindComponents = () => {
+  return (
+    <div>
+      <h1 className="text-3xl">tailwind</h1>
+      <button className="bg-blue-100 rounded-xl px-2">Button</button>
+    </div>
+  );
+};
+
+export default TailWindComponents;
