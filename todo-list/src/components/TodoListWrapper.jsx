481c74dde3c02cd9efac78a17000c2d68303c121
kimyoungseok11 <emaonx2@gmail.com>
2024-02-25 17:05:35 +0900

    24-02-25 todolist crud 기능 추가

--- todo-list/src/components/TodoListWrapper.jsx
+++ todo-list/src/components/TodoListWrapper.jsx
@@ -1,23 +1,45 @@
 import React, { useReducer, useState } from "react";
 import style from "../css/TodoListWrapper.module.css";
 import todoListReducer from "../reducer/todoListReducer";
+import { FaTrashAlt } from "react-icons/fa";
 
 const TodoListWrapper = () => {
-  const initialList = [{ name: "ddd" }];
+  const initialList = [{ index: 1, name: "ddd", isChecked: false }];
   const [todoItems, dispatch] = useReducer(todoListReducer, initialList);
   const [addItem, setAddItem] = useState("");
   const handleInputChnage = (e) => {
     setAddItem(e.target.value);
   };
   const handleAddClick = () => {
-    dispatch({ type: "add", addItem });
+    dispatch({ type: "add", todoItems, addItem });
+    setAddItem("");
+  };
+  const handleCheckBox = (isChecked, index) => {
+    dispatch({ type: "update", todoItems, isChecked, index });
+    // dispatch({});
+  };
+  const handleTrashCanClick = (index) => {
+    dispatch({ type: "delete", todoItems, index });
   };
   return (
     <div className={style.listWrapper}>
       <div className={style.listBox}>
-        {todoItems.map((item, index) => (
-          <div key={index} className={style.todoItem}>
-            {item.name}
+        {todoItems.map((item) => (
+          <div key={item.index} className={style.todoList}>
+            <div className={style.todoItems}>
+              <input
+                type="checkbox"
+                onChange={() => {
+                  handleCheckBox(item.isChecked, item.index);
+                }}
+              ></input>
+              {item.name}
+            </div>
+            <FaTrashAlt
+              onClick={() => {
+                handleTrashCanClick(item.index);
+              }}
+            />
           </div>
         ))}
       </div>
--- todo-list/src/reducer/todoListReducer.js
+++ todo-list/src/reducer/todoListReducer.js
@@ -1,9 +1,34 @@
-export default function todoListReducer(todoItems, action) {
+export default function todoListReducer(prevItem, action) {
   switch (action.type) {
     case "add": {
+      const { addItem } = action;
+      let newIdx;
+      if (prevItem.length === 0) {
+        newIdx = 0;
+      } else {
+        newIdx = prevItem[prevItem.length - 1].index + 1;
+      }
+      return [...prevItem, { index: newIdx, name: addItem, isChecked: false }];
     }
-    case "update":
-    case "delete":
+    case "update": {
+      const { isChecked, index } = action;
+      const newItems = prevItem.map((item) => {
+        if (item.index === index) {
+          return { ...item, isChecked: !isChecked };
+        } else {
+          return { ...item };
+        }
+      });
+      return [...newItems];
+    }
+    case "delete": {
+      const { index } = action;
+
+      const newItems = prevItem.filter((item) => {
+        return item.index !== index;
+      });
+      return [...newItems];
+    }
     case "default": {
       throw Error(`알 수 없는 action type ; ${action.type}`);
     }
